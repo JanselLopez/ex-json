57cc88c1a0ad74a9adfc71ca294ca4c1197cdf5e
Jesus Reikel Lopez Martin <jesus.reikel@gmail.com>
2023-02-28 14:32:41 -0500

    feat(env) add env config

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const Pool = require ('pg').Pool;
-const pool = new Pool ({
-  user: 'postgres',
-  password: 'FzZ3v8vZLpu3kQG',
-  database: 'json_express_db',
-  host: 'localhost',
-  port: '5433',
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-const express = require ('express');
-const pool = require ('./db');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const pool = require("./db");
 const PORT = 3010;
 
-const app = express ();
+const app = express();
 
-app.use (express.json ());
+app.use(express.json());
 
-app.post ('/config', async (req, res) => {
+app.post("/config", async (req, res) => {
   try {
-    const {models, GET} = req.body;
-    createModels (models);
-    createGetters (GET);
-    res.send ('Config created');
+    const { models, GET } = req.body;
+    createModels(models);
+    createGetters(GET);
+    res.send("Config created");
   } catch (error) {
-    res.json ({
+    res.json({
       error: error.message,
     });
   }
 });
 
-app.listen (PORT, async () => {
-  console.log (`listening in port ${PORT}`);
+app.listen(PORT, async () => {
+  console.log(`listening in port ${PORT}`);
 });
 
-const createGetters = GET => {
-  Object.keys (GET).forEach (endpoint => {
-    app.get (endpoint, async (req, res) => {
-      const values = GET[endpoint].split ('-');
+const createGetters = (GET) => {
+  Object.keys(GET).forEach((endpoint) => {
+    app.get(endpoint, async (req, res) => {
+      const values = GET[endpoint].split("-");
       let createEndpointQuery = `SELECT * FROM ${values[0]} `;
       if (values.length > 1) {
-        values.forEach ((v, i) => {
+        values.forEach((v, i) => {
           if (i > 0) {
             if (i == 1) {
               createEndpointQuery += `WHERE ${v} = ${req.query[v]} `;
@@ -39,35 +42,37 @@ const createGetters = GET => {
           }
         });
       }
-      createEndpointQuery += ';';
-      console.log (createEndpointQuery);
-      const get = await pool.query (createEndpointQuery, []);
-      res.json (get.rows);
+      createEndpointQuery += ";";
+      console.log(createEndpointQuery);
+      const get = await pool.query(createEndpointQuery, []);
+      res.json(get.rows);
     });
   });
 };
 
-const createModels = models => {
-  Object.keys (models).forEach (modelName => {
+const createModels = (models) => {
+  Object.keys(models).forEach((modelName) => {
     let createTableQuery = `CREATE TABLE ${modelName} (
-        ${Object.keys (models[modelName]).map (attrName => {
-          const processed = models[modelName][attrName].split ('-');
-          return `${attrName} ${getType (processed[0])} ${processed[1] == 'PK' ? 'PRIMARY KEY' : ''}`;
+        ${Object.keys(models[modelName]).map((attrName) => {
+          const processed = models[modelName][attrName].split("-");
+          return `${attrName} ${getType(processed[0])} ${
+            processed[1] == "PK" ? "PRIMARY KEY" : ""
+          }`;
         })}
     )`;
-    createTableQuery += ';';
-    console.log (createTableQuery);
-    pool.query (createTableQuery, []);
+    createTableQuery += ";";
+    console.log(createTableQuery);
+    pool.query(createTableQuery, []);
   });
 };
 
-const getType = type => {
+const getType = (type) => {
   switch (type) {
-    case 'int':
-      return 'INTEGER';
-    case 'boolean':
-      return 'BOOLEAN';
+    case "int":
+      return "INTEGER";
+    case "boolean":
+      return "BOOLEAN";
     default:
-      return 'TEXT';
+      return "TEXT";
   }
 };
